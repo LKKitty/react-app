62509f60e1475e430e25983660c7b35691023a6f
LKKitty <LKKitty@git.com>
2019-01-08 11:37:35 +0800

    完成了第一阶段评论功能

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,9 +1,61 @@
 import React, {Component} from 'react'
 
 class CommentInput extends Component {
+  constructor (){
+    super()
+    this.state = {
+      username: '',
+      content: ''
+    }
+  }
+  // 输入用户名
+  handleUsernameChange (event) {
+    this.setState({
+      username:event.target.value
+    })
+  }
+  //输入评论内容
+  handleContentChange (event) {
+    this.setState({
+      content:event.target.value
+    })
+  }
+  // 点击提交
+  handleSubmit (event) {
+    if(this.props.onSubmit){
+      const {username,content} = this.state
+      this.props.onSubmit({username,content})
+    }
+    this.setState({content:''})
+  }
   render() {
     return (
-      <div>CommentInput</div>
+      <div className='comment-input'>
+        <div className='comment-field'>
+          <span className='comment-field-name'>用户名：</span>
+          <div className='comment-field-input'>
+            <input 
+            value={this.state.username}
+            onChange={this.handleUsernameChange.bind(this)}
+            />
+          </div>
+        </div>
+        <div className='comment-field'>
+          <span className='comment-field-name'>评论内容：</span>
+          <div className='comment-field-input'>
+            <textarea
+            value={this.state.content}
+            onChange={this.handleContentChange.bind(this)}
+            />
+          </div>
+        </div>
+        <div className='comment-field-button'>
+          <button 
+            onClick={this.handleSubmit.bind(this)}>
+            发布
+          </button>
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,77 +1,86 @@
-import React,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React,{Component} from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-class LikeButton extends Component {
-  static defaultProps = {
-    likedText: '取消',
-    unlikedText: '点赞'
-  }
-  constructor (){
-    super()
-    this.state = {isLiked: false}
-  }
-  handleClickOnLikeButton () {
-    this.setState({isLiked: !this.state.isLiked})
-    if(this.props.onClick) {
-      this.props.onClick()
-    }
-  }
-  render () {
-    return (
-      <button onClick={this.handleClickOnLikeButton.bind(this)}>
-          {this.state.isLiked ? this.props.likeText : this.props.unlikeText} 👍
-      </button>
-    )
-  }
-}
-const users = [
-  { username: 'Jerry', age: 21, gender: 'male' },
-  { username: 'Tomy', age: 22, gender: 'male' },
-  { username: 'Lily', age: 19, gender: 'female' },
-  { username: 'Lucy', age: 20, gender: 'female' }
-]
-class User extends Component {
-  render (){
-    const {user} = this.props
-    console.log(user)
-    return (
-      <div>
-        <div>姓名：{user.username}</div>
-        <div>年龄：{user.age}</div>
-        <div>性别：{user.gender}</div>
-        <hr/>
-      </div>
-    )
-  }
-}
-class Index extends Component {
-  constructor (){
-    super()
-    this.state = {
-      likedText: '已赞',
-      unlikedText: '赞'
-    }
-  }
-  handleClickOnChange(){
-    this.setState({
-      likedText: '取消',
-      unlikedText: '点赞'
-    })
-  }
-  render () {
-    return (
-            <div>
-              {users.map((user,i) => <User key={i} user={user} />)}
-            </div>
-          )
-  }
-}
-ReactDOM.render(<Index />, document.getElementById('root'));
+// class LikeButton extends Component {
+//   static defaultProps = {
+//     likedText: '取消',
+//     unlikedText: '点赞'
+//   }
+//   constructor (){
+//     super()
+//     this.state = {isLiked: false}
+//   }
+//   handleClickOnLikeButton () {
+//     this.setState({isLiked: !this.state.isLiked})
+//     if(this.props.onClick) {
+//       this.props.onClick()
+//     }
+//   }
+//   render () {
+//     return (
+//       <button onClick={this.handleClickOnLikeButton.bind(this)}>
+//           {this.state.isLiked ? this.props.likeText : this.props.unlikeText} 👍
+//       </button>
+//     )
+//   }
+// }
+// const users = [
+//   { username: 'Jerry', age: 21, gender: 'male' },
+//   { username: 'Tomy', age: 22, gender: 'male' },
+//   { username: 'Lily', age: 19, gender: 'female' },
+//   { username: 'Lucy', age: 20, gender: 'female' }
+// ]
+// class User extends Component {
+//   render (){
+//     const {user} = this.props
+//     console.log(user)
+//     return (
+//       <div>
+//         <div>姓名：{user.username}</div>
+//         <div>年龄：{user.age}</div>
+//         <div>性别：{user.gender}</div>
+//         <hr/>
+//       </div>
+//     )
+//   }
+// }
+// class Index extends Component {
+//   constructor (){
+//     super()
+//     this.state = {
+//       likedText: '已赞',
+//       unlikedText: '赞'
+//     }
+//   }
+//   handleClickOnChange(){
+//     this.setState({
+//       likedText: '取消',
+//       unlikedText: '点赞'
+//     })
+//   }
+//   render () {
+//     return (
+//             <div>
+//               {users.map((user,i) => <User key={i} user={user} />)}
+//             </div>
+//           )
+//   }
+// }
+// ReactDOM.render(<Index />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './index.css'
+
+ReactDOM.render(
+  <CommentApp />,
+  document.getElementById('root')
+)
