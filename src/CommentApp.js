ddb3abe79ef5d98fee0dd3b789a07f11be08dcf8
LKKitty <LKKitty@git.com>
2019-01-11 13:45:44 +0800

    望

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -9,14 +9,34 @@ class CommentApp extends Component {
       comments: []
     }
   }
+  // 加载前
+  componentWillMount() {
+    this._loadComments()
+  }
+  // 加载存储列表
+  _loadComments () {
+    let comments =localStorage.getItem('comments')
+    // console.log(comments)
+    if(comments) {
+      comments = JSON.parse(comments)
+      this.setState({comments})
+    }
+  }
+  // 存储列表
+  _saveComments (comments) {
+    localStorage.setItem('comments',JSON.stringify(comments))
+  }
+  // 点击提交
   handleSubmitComment (comment) {
     if(!comment) return
     if(!comment.username) return alert('请输入用户名')
     if(!comment.content) return alert('请输入评论内容')
-    this.state.comments.push(comment)
+    const comments = this.state.comments
+    comments.push(comment)
     this.setState({
-      comments: this.state.comments
+      comments
     })
+    this._saveComments(comments)
   }
   render() {
     return (
